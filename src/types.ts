8f2d9873b7b1375bacc7e4198b67eda440653473
Rahmux213 <boudrarahma03@gmail.com>
2026-03-18 17:51:59 +0100

    Define Language type and Book/Translations interfaces
    Added Language type and Book, Translations interfaces.

--- src/types.ts
+++ src/types.ts
@@ -1,1 +1,23 @@
 
+export type Language = 'fr' | 'es';
+
+export interface Book {
+  id: string;
+  title: string;
+  author: string;
+  description: {
+    fr: string;
+    es: string;
+  };
+  image: string;
+  year: string;
+  category: string;
+  price?: number;
+}
+
+export interface Translations {
+  [key: string]: {
+    fr: string;
+    es: string;
+  };
+}
